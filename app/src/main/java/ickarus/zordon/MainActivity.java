8dda08b075a3c154460faff812f4dd36cad801be
rcidigo <richardcidigo@gmail.com>
2016-02-02 20:22:05 -0800

    added a few new activities, working on look dashboard

--- app/src/main/java/ickarus/zordon/MainActivity.java
+++ app/src/main/java/ickarus/zordon/MainActivity.java
@@ -22,20 +22,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-
-            }
-        });
 
         Button sbutton = (Button)findViewById(R.id.signupbutton);
         sbutton.setOnClickListener(new View.OnClickListener() {
-            @Override
+            @Override 
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, SignUpActivity.class));
             }
