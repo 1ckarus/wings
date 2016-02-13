8cfa7ea846c36b4df6ef77071f8ac5aa406e5619
rcidigo <richardcidigo@gmail.com>
2016-02-13 12:21:30 -0800

    updated the menu drawer to come out

--- app/src/main/java/ickarus/zordon/dashBoard.java
+++ app/src/main/java/ickarus/zordon/dashBoard.java
@@ -1,11 +1,13 @@
 package ickarus.zordon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class dashBoard extends AppCompatActivity {
 
@@ -17,6 +19,18 @@ public class dashBoard extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        Button menuButton= (Button) findViewById(R.id.menuButton);
+
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(dashBoard.this, menuDrawer.class));
+            }
+
+        });
     }
+
 
 }
